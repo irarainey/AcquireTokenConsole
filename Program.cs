988c454edfc77e724e17abb512826c3e46e37c43
Ira Rainey <irarainey@microsoft.com>
2022-11-03 09:53:59 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Identity.Client;
+
+string _tenant_id = "";
+string _client_id = "";
+string _redirect_uri = "";
+string[] _scopes = new[] { "" };
+
+var clientApp = PublicClientApplicationBuilder
+            .Create(_client_id)
+            .WithTenantId(_tenant_id)
+            .WithRedirectUri(_redirect_uri)
+            .Build();
+
+AuthenticationResult authResult = await clientApp
+    .AcquireTokenInteractive(_scopes)
+    .ExecuteAsync();
+
+Console.WriteLine(authResult.AccessToken);
